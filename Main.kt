e3170c371137bd6f28a3ce9e59f7cd0b983a32f2
rudyten10 <94325541+rudyten10@users.noreply.github.com>
2021-12-08 06:34:39 -0800

    Stage 2/6: Ingredient calculator
    Stage 2/6: Ingredient calculator

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 package machine
 
-// Stage 1/6: Making coffee
+// Stage 2/6: Making coffee
 fun main() {
    println("Starting to make a coffee")
    println("Grinding coffee beans")
@@ -9,4 +9,27 @@ fun main() {
    println("Pouring coffee into the cup")
    println("Pouring some milk into the cup")
    println("Coffee is ready!")
+
+
+
+   println("Write how many cups of coffee you will need:")
+   val cups = readLine()!!.toInt()
+
+   val list = calcIngredientsQty(cups)
+
+   println("For $cups cups of coffee you will need:")
+   println("${list[0]} ml of water") // water
+   println("${list[1]} ml of milk") // milk
+   println("${list[2]} g of coffee beans") // beans
+}
+
+fun calcIngredientsQty(cups: Int = 1): List<Int>{
+   val water = 200 // ml
+   val milk = 50 // ml
+   val coffeeBeans = 15 // g
+
+
+   val list = listOf(water*cups, milk*cups, coffeeBeans*cups)
+
+   return list
 }
